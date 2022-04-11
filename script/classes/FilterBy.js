1e069f18cc3fe96574b5e86afbb6764862821978
jbades <jbades@majba.com>
2022-04-11 09:47:27 +0200

    Class FilterBy

---
+++ script/classes/FilterBy.js
@@ -0,0 +1,57 @@
+export default class FilterBy
+{
+    constructor(gallery)
+    {
+        this.gallery = gallery;
+        this.all = new Set();
+        this.filterName = {
+            ingredient: 'Ingrédients',
+            appliance: 'Appareils',
+            ustensils: 'Ustensils'
+        }
+        this.filterPlaceholder = {
+            ingredient: 'Rechercher un ingrédient',
+            appliance: 'Rechercher un appareil',
+            ustensils: 'Rechercher un ustensil'
+        }
+    }
+
+    buildDropdown(item)
+    {
+        let html = 
+        `
+            <div class="btn-wrapper bg-primary text-white">
+                <div class="d-flex flex-row flex-nowrap justify-content-between align-items-center filter-btn">
+                    <span>Ingrédients</span>
+                    <i class="fa fa-chevron-down" aria-hidden="true"></i> 
+                </div>
+                <div class="d-flex flex-column flex-nowrap hidden-btn">
+                    <div class="d-flex flex-row flex-nowrap justify-content-between align-items-center">
+                        <input
+                            id="search-${item}>"
+                            name="search-${item}"
+                            class=""
+                            placeholder="Rechercher un ingrédient"
+                        />
+                        <i class="fa fa-chevron-up" aria-hidden="true"></i> 
+                    </div>
+                    <div class="">lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor</div>
+                </div>
+            </div>
+        `
+        
+        document.querySelector('.filterbar').innerHTML = html;
+    }
+
+    collect()
+    {
+        this.gallery.all.forEach(recipe => 
+        {
+            recipe.ingredients.forEach(ingObject => 
+                {
+                    this.all.add(ingObject.ingredient);
+                });
+        });
+        console.log(this.all);
+    }
+}
--- script/pages/index.js
+++ script/pages/index.js
@@ -1,10 +1,11 @@
 import { recipes } from "../../data/recipes.js"
 import RecipesGallery from "../classes/RecipesGallery.js"
-import FilterByIngredient from "../classes/FilterByIngredient.js"
+import FilterBy from "../classes/FilterBy.js"
 
 let gallery = new RecipesGallery();
 gallery.hydrate(recipes);
 gallery.display();
-let filter = new FilterByIngredient(gallery);
-console.log(filter);
+let filter = new FilterBy(gallery);
+filter.buildDropdown('ingredient');
+filter.buildDropdown('appliance');
 filter.collect();
