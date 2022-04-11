ecfb81ad51c0807a25850287544dd8270f6bb742
jbades <jbades@majba.com>
2022-04-11 09:49:38 +0200

    idem

--- script/classes/FilterBy.js
+++ script/classes/FilterBy.js
@@ -22,7 +22,7 @@ export default class FilterBy
         `
             <div class="btn-wrapper bg-primary text-white">
                 <div class="d-flex flex-row flex-nowrap justify-content-between align-items-center filter-btn">
-                    <span>Ingrédients</span>
+                    <span>${this.filterName[item]}</span>
                     <i class="fa fa-chevron-down" aria-hidden="true"></i> 
                 </div>
                 <div class="d-flex flex-column flex-nowrap hidden-btn">
@@ -31,7 +31,7 @@ export default class FilterBy
                             id="search-${item}>"
                             name="search-${item}"
                             class=""
-                            placeholder="Rechercher un ingrédient"
+                            placeholder="${this.filterPlaceholder[item]}"
                         />
                         <i class="fa fa-chevron-up" aria-hidden="true"></i> 
                     </div>
